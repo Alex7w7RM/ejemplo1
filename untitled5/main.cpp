b1315d2ac5e081b75adfc04944ce6f874453fc0a
Alex7w7RM <90742335+Alex7w7RM@users.noreply.github.com>
2024-02-26 12:00:06 -0600

    tabla del 4

---
+++ untitled5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ using namespace std;
+
+int a =1 , b= 1, resu1 , resu2;
+int main() {
+
+    cout << " tabla del 4 " << endl ;
+
+
+
+    do {
+        resu1 = 4 * a;
+        cout << 4 << "*" << a  << "=" << resu1 << endl;
+        a++;
+    } while (a <=10 );
+
+ 
+
+    return 0;
+}
